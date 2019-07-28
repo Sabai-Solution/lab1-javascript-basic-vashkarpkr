67186c1506e9e1b3fcf3bd9dc3238503805c2812
vashkarpkr <vashkarpkr@gmail.com>
2019-07-28 15:47:35 +0545

    code added

---
+++ s.js
@@ -0,0 +1,33 @@
+function submitFuntion() {
+	var emailText=document.getElementsByName("Email")[0].value;
+	var userText=document.getElementsByName("user")[0].value;
+	var passText=document.getElementsByName("pass")[0].value;
+
+	var append = "";
+	if(emailText=""){
+		append = "Email";
+
+	}
+	if (userText="") {
+		if(append!=""){
+			append = append + " and";
+		}
+		append= append + " username";
+	}
+	if (passText="") {
+		if(append!=""){
+			append = append + " and";
+		}
+		append= append + " password";
+	}
+	append = append + "cannot be empty";
+
+
+if(append!=""){
+	alert(append);
+	return false;
+}
+else{
+	return  true;
+}
+}
